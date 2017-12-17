2181cb4ba2994b4f629f42a192e1e086857e350f
Ana Izabel <jacobsenanaizabel@gmail.com>
2017-12-17 02:49:45 -0200

    route getPlans

--- app/controllers/main.controller.js
+++ app/controllers/main.controller.js
@@ -1,15 +1,25 @@
-app.controller('MainController', function($scope, $location, MainService) {
+app.controller('MainController', function($scope, $location, MainService, $rootScope) {
 
-    var self = this;
     $scope.platforms = [];
+    $rootScope.plans = [];
 
-    $scope.plansDetail = function(type){
-        $location.path("/plans/" + type);
+    $scope.plansDetail = function(platform){
+        MainService.getPlain(platform).then(function(response) {
+            _.each(response.planos, function(plan){
+                $rootScope.plans.push(plan);
+            })
+        });
+
+        $location.path("/plans/" + platform);
     }; 
 
+    console.log($rootScope.plans);
+    
     $scope.init = function(){
         MainService.getPlatforms().then(function(response) {
             _.each(response.plataformas, function(platform){
+                platform.descricao = platform.descricao.replace("|"," ").toLowerCase();
+                platform.nome = platform.nome.toLowerCase();
                 $scope.platforms.push(platform);
             })
         });
--- app/services/main.service.js
+++ app/services/main.service.js
@@ -19,5 +19,36 @@ app.service('MainService', function($http,$q, $log) {
             console.log(e, "can not get data.");
         });
     };
+    
+    self.getPlain = function (platformChoose) {
+
+        switch(platformChoose){
+            case 'computador':
+                return $http
+                .get(endpoints.computer)
+                .then(function(response) {
+                    return response.data;
+                }, function(e) {
+                    console.log(e, "can not get data.");
+                });
+            case 'tablet':
+                return $http
+                .get(endpoints.tablet)
+                .then(function(response) {
+                    return response.data;
+                }, function(e) {
+                    console.log(e, "can not get data.");
+                });
+            case 'wi-fi':
+                return $http
+                .get(endpoints.wifi)
+                .then(function(response) {
+                    return response.data;
+                }, function(e) {
+                    console.log(e, "can not get data.");
+                });
+        }
+    };
+
 
 });
